d9ec388a6ba1561e026554c8aa440c81538c34fa
Michael Alexander <michael.alexander@glasgow.ac.uk>
2022-04-20 10:58:29 +0100

    Rework into EmissionsDB class

--- 2022/solution.py
+++ 2022/solution.py
@@ -7,6 +7,118 @@ from argparse import ArgumentParser
 from Database import Database
 
 
+class EmissionsDB(Database):
+    '''Database for analysis of per-capita emissions.'''
+
+    def __init__(self, *args, **kwargs):
+        '''Takes the same arguments as Database.Database.__init__.'''
+        # Initialise the base class
+        super(EmissionsDB, self).__init__(*args, **kwargs)
+
+        # Check that the DB has the expected columns
+        expected = ('Country Name', 'Region', 'IncomeGroup')
+        columns = list(self.Entry.attributes())
+        # First the non-year columns
+        for column in expected:
+            try:
+                columns.remove(column)
+            except ValueError:
+                raise ValueError(f'Expected column {column!r} is missing!')
+        # All remaining columns should be years
+        for column in columns:
+            if not column.isnumeric():
+                raise ValueError(('Expected only year columns in addition'
+                                  ' to {expected}, but found a column'
+                                  ' {column!r}').format(expected=expected,
+                                                        column=column))
+        # Tuple of years
+        self.years = tuple(columns)
+
+    # We don't need to access the DB in this function, but logically
+    # it belongs to the DB class, so make it a static method
+    @staticmethod
+    def sum_emissions(entry, years):
+        '''Sum the emissions of the entry over the given years.'''
+        return sum(entry[i] for i in years
+                   if entry[i] != '')
+
+    def summed_emissions(self, years=None):
+        '''Get the total emissions of all countries over the given years
+        (default all years). Returns a dict {country : sum}.'''
+        if years is None:
+            years = self.years
+        # Call the static method on the class rather than self to make
+        # it clear that it's a static method being called
+        return {entry['Country Name'] : EmissionsDB.sum_emissions(entry, years)
+                for entry in self}
+
+    def get_grouped_DB(self, group):
+        '''Make a DB of countries grouped by the column 'group' 
+        (eg, Region).'''
+        # For each value of the group column, make a dict with the expected
+        # column values. 'Country Name' is swapped for the group, 'Region'
+        # and 'IncomeGroup' are set to null. Initialise the emissions
+        # for each year to zero.
+        groups = {}
+        for gr in set(self.iterator(group)):
+            initvals = {'Country Name' : gr,
+                        'Region' : '',
+                        'IncomeGroup' : ''}
+            for year in self.years:
+                initvals[year] = 0.
+            groups[gr] = initvals
+        for entry in self:
+            # Get the group for this entry
+            entryGroup = groups[entry[group]]
+            # Loop over years and add non-null values to the emissions
+            # for the group.
+            for year in self.years:
+                if entry[year] == '':
+                    continue
+                entryGroup[year] += entry[year]
+        # Make Database.Entry instances for each group
+        grEntries = [self.Entry(**vals) for vals in groups.values()]
+        # Make a DB of the entries
+        grDB = EmissionsDB(grEntries)
+        return grDB
+
+    def sum_and_sort_emissions(self, years=None, output=True):
+        '''Sum emissions over the given years for the entries in the database,
+        then sort by total emissions.'''
+        if years is None:
+            years = self.years
+        # Get total emissions for each country for the given years
+        sums = {entry['Country Name'] : EmissionsDB.sum_emissions(entry, years)
+                for entry in self}
+        # Sort by the total emissions to get a list of pairs of (country, sum)
+        sums = sorted(sums.items(), key=lambda x : x[1],
+                      reverse=True)
+        # Print if requested
+        if output:
+            for name, tot in sums:
+                print('{0:<30}: {1:.2f}'.format(name, tot))
+        return sums
+
+    def sum_and_sort_per_decade(self, output=True):
+        '''Sum emissions for each entry and sort by total emissions,
+        for each decade.'''
+        # Take the first 3 digits of the years to get the decades
+        # eg, 1967 -> 196.
+        decades = sorted(set(year[:-1] for year in self.years))
+        decadeSums = {}
+        for decade in decades:
+            # The years for each decade are all those starting with
+            # the same 3 digits.
+            decadeYears = list(filter(lambda year : year.startswith(decade),
+                                      self.years))
+            if output:
+                print('Decade:', decade + '0')
+            # Sum and sort for each decade, and print the results
+            sums = self.sum_and_sort_emissions(decadeYears, output)
+            decadeSums[decade + '0'] = sums
+        return decadeSums
+
+
 def prob1(db):
     '''Find the countries with the lowest and highest emissions in 1960,
     and similarly in 2018 (ignoring countries with no data for the given year).
@@ -27,25 +139,12 @@ def prob2(db):
         db.print_stats(year)
 
 
-def get_years(db):
-    '''Get a tuple of year columns in the DB.'''
-    return tuple(column for column in db.Entry.attributes() if column.isnumeric())
-
-
-def sum_emissions(entry, years):
-    '''Sum the emissions of the entry over the given years.'''
-    return sum(entry[str(i)] for i in years
-               if entry[str(i)] != '')
-
-
 def prob3(db):
     '''Find the countries with the lowest and highest total emissions
     summed over all years for which there's data. Print their names and
     total emissions.'''
     # Sum the emissions over years for each country
-    years = get_years(db)
-    sums = {entry['Country Name'] : sum_emissions(entry, years)
-            for entry in db}
+    sums = db.summed_emissions()
     # Find the min & max
     mincountry = min(sums.items(), key=lambda entry : entry[1])
     maxcountry = max(sums.items(), key=lambda entry : entry[1])
@@ -53,44 +152,11 @@ def prob3(db):
     print('Max. total emissions: {0} {1:.2f}'.format(*maxcountry))
 
 
-def make_grouped_DB(db, group):
-    '''Group entries by their values of 'group' and sum together their emissions.'''
-    groups = {}
-    years = get_years(db)
-    for gr in set(db.iterator(group)):
-        initvals = {'Country Name' : gr,
-                    'Region' : '',
-                    'IncomeGroup' : ''}
-        for year in years:
-            initvals[year] = 0.
-        groups[gr] = initvals
-    for entry in db:
-        for year in years:
-            if entry[year] == '':
-                continue
-            groups[entry[group]][year] += entry[year]
-    grentries = [db.Entry(**vals) for vals in groups.values()]
-    grdb = Database(grentries)
-    return grdb
-
-
-def sum_and_sort_emissions(db, years, output=True):
-    '''Sum emissions over the given years for the entries in the database,
-    then sort by total emissions.'''
-    sums = {entry['Country Name'] : sum_emissions(entry, years)
-            for entry in db}
-    if output:
-        for name, tot in sorted(sums.items(), key=lambda x : x[1],
-                                reverse=True):
-            print('{0:<30}: {1:.2f}'.format(name, tot))
-    return sums
-
-
 def grouped_DBs(db, *groups):
     '''Make DBs grouped by the given column names.'''
     dbs = {}
     for group in groups:
-        grdb = make_grouped_DB(db, group)
+        grdb = db.get_grouped_DB(group)
         dbs[group] = grdb
     return dbs
 
@@ -100,10 +166,9 @@ def prob4(grDBs):
     Print a list of the regions and their total emissions, ordered by their 
     emissions. Do the same grouping the countries by income group.'''
     # Get the years from the first DB
-    years = get_years(list(grDBs.values())[0])
     for group, grDB in grDBs.items():
         print('Grouped by', group, ':')
-        sum_and_sort_emissions(grDB, years)
+        grDB.sum_and_sort_emissions()
         print()
 
 
@@ -114,19 +179,13 @@ def prob5(grDBs):
     sorted by emissions. Do the same grouping the countries by income group.'''
     for group, db in grDBs.items():
         print('Group by', group)
-        years = get_years(db)
-        decades = sorted(set(year[:-1] for year in years))
-        for decade in decades:
-            decadeyears = list(filter(lambda year : year.startswith(decade),
-                                      years))
-            print('Decade:', decade + '0')
-            sum_and_sort_emissions(db, decadeyears)
+        db.sum_and_sort_per_decade()
         print()
 
 
 def main(inputfile, *problems):
     '''Solve the assignment problems.'''
-    db = Database(csvfile=inputfile)
+    db = EmissionsDB(csvfile=inputfile)
     grDBs = None
     if '4' in problems or '5' in problems:
         grDBs = grouped_DBs(db, 'Region', 'IncomeGroup')
@@ -138,7 +197,7 @@ def main(inputfile, *problems):
     for prob in sorted(problems):
         func, arg = funcs[prob]
         print('*** Problem', prob)
-        print('***', func.__doc__)
+        print('   ', func.__doc__)
         func(arg)
         print()
 
